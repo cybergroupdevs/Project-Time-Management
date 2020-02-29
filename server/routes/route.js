626309ba0815c193754a10b288a57935aeb4af26
balani98 <balanideepanshu92@gmail.com>
2020-02-29 17:29:59 +0530

    some minor changes in employee API

--- server/routes/route.js
+++ server/routes/route.js
@@ -3,6 +3,11 @@ const controller = require('../controllers');
 module.exports= (app) => {
 	
 	app.post("/login", controller.login.checkUserAuthentication);
+	app.get("/employees",controller.employees.index);
+	app.post("/employees",controller.employees.create);
+	app.put("/employees/:id",controller.employees.update);
+	app.delete("/employees/:id",controller.employees.delete);
+	app.get("employees/:id",controller.employees.show);
 	app.get("/projects",controller.projects.index);
 	app.post("/projects",controller.projects.create);
 	app.get("/projects/:id",controller.projects.show);
